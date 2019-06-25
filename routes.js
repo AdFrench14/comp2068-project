c9eda4f9b6e1bc09835d22d52c1b3952beeed907
belbin09 <200232303@student.georgianc.on.ca>
2019-06-25 10:50:31 -0400

    appart changes never pushed

--- routes.js
+++ routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 const pageRoutes = require('./routes/pageRoutes');
+const conversationRoutes = require('./routes/conversationRoutes');
 
 app.use('/', pageRoutes);
+app.use('/conversations', conversationRoutes);
 
 module.exports = app;
