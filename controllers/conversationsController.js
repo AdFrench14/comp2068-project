52b7c72ce7298ed43bdaca855aaad0a7da6cd637
Adrien <adrien@pop-os.localdomain>
2019-06-27 20:30:17 -0400

    removed unecessary comments'

--- controllers/conversationsController.js
+++ controllers/conversationsController.js
@@ -34,41 +34,6 @@ exports.create = (req, res) => {
             req.flash('error', `Could not find the recipient: ${err}`);
             res.redirect('/conversations/new');
     });
-   /* 
-    Conversation.create(new Conversation({
-                users: [req.session.userId, recipient._id],
-                message: [] //empty conversation
-            }))
-                .then(()=> {
-                    req.flash('success', "Conversation created successfully");
-                    console.log("Conversation created successfully");
-                    req.redirect('/conversations'); //should probably open the new conversation instead
-                })
-                .catch(err => {
-                    req.flash('err', `${err}`);
-                    res.redirect('/conversations/new');
-                });
-                */
-    //temp conversation object that can be written to the DB
-    /*j
-    var conversation = new Conversation( {
-        users: [req.session.userId, recipientId],
-        messages: []
-    });
-    */
-    /*
-    //Conversation.create(req.body.conversation)
-    Conversation.create(conversation)
-        .then(() => {
-            req.flash('success', "Conversation created successfully");
-            console.log("Conversation created successfully");
-            req.redirect('/conversations'); //should probably open the new conversation instead
-        })
-        .catch(err => {
-            req.flash('error', `Error: ${err}`);
-            res.redirect('/conversations');
-        });
-        */
 }
 
 exports.index = (req, res) => {
@@ -76,33 +41,7 @@ exports.index = (req, res) => {
         //conversations in which the current user is a participant
         users: {$elemMatch: {$in: [req.session.userId]}}
     })
-    /*
-        .then(conversations => {
-            //query for the names of the users involved in this conversation
-            console.log("Current session userId: " + req.session.userId);
-            
-            //get all the user ids that have been involved in a conversation
-            userIds = [];
-            for(let i = 0; i < conversations.length; i++) {
-                userIds.push(conversations[i].users);
-            }
-            console.log('users w duplicated: ' + userIds);
-            const uniqueUserIds = [... new Set(userIds)]; //remove duplicates by passing thorugh a Set
-            console.log('unique users: ' + uniqueUserIds);
-
-            //Find the corresponding user objects in the db
-            User.find({
-                _id: {$in: uniqueUserIds}
-            })
-                .then(users => {
-                    return conversations, users;
-                })
-                .catch(err => {
-                    console.log(`Could not find the conversation's users. ${err}`);
-                });
-        })
-        */
-        .populate('users')
+           .populate('users')
         .then((conversations) => {
             res.render('conversations/index', {
                 conversations: conversations,
@@ -120,17 +59,6 @@ exports.index = (req, res) => {
 exports.show = (req, res) => {
     res.redirect('/messages'); //this actually redirects to /conversations/messages
 }
-
-/*
-//don't think we need these, shouldn't be able to edit a converstaion
-exports.edit = (req, res) => {
-
-}
-
-exports.update = (req, res) => {
-
-}
-*/
 
 exports.destroy = (req, res) => {
     Conversation.deleteOne({
